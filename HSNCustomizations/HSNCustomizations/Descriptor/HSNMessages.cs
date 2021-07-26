a76476c61624c11b7ae68f1dbeabd79019f9405b
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-07-26 04:13:04 +0000

    Add several constant string texts.

--- HSNCustomizations/HSNCustomizations/Descriptor/HSNMessages.cs
+++ HSNCustomizations/HSNCustomizations/Descriptor/HSNMessages.cs
@@ -11,11 +11,13 @@ namespace HSNCustomizations.Descriptor
         public const string RMAInitiated      = "RMA Initiated";
         public const string ReturnRMA         = "Return RMA";
         public const string RMAReturned       = "RMA Returned";
+        public const string RMAReceipted      = "RMA Receipted";
         public const string NonUniqueSerNbr   = "The Serial Number Isn't Unique For This Equipment Type.";
         public const string ApptLineTypeInvt  = "The Button Is Disabled Because Line Type Isn't Invetory Item";
         public const string InvtTranNoAllRlsd = "There Are Related Inventory Transactions Of This Appointment Are Not Yet Released";
         public const string UnitCostIsZero    = "The Unit Cost Is 0, Please Double Check.";
         public const string InitRMANotCompl   = "The Initiate RMA Receipt Not Yet Complete.";
         public const string NoInitRMARcpt     = "You Must Initiate RMA Process For The Inventory.";
+        public const string MustReturnRMA     = "Please Return The RMA.";
     }
 }
