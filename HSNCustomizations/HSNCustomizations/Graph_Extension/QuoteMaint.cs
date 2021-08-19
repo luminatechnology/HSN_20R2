4cb98431890d962b624e3f6bc296f6f0a64165f5
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-08-19 07:31:57 +0000

    Add custom report.

--- HSNCustomizations/HSNCustomizations/Graph_Extension/QuoteMaint.cs
+++ HSNCustomizations/HSNCustomizations/Graph_Extension/QuoteMaint.cs
@@ -1,14 +1,45 @@
 using PX.Data;
 using PX.Data.BQL.Fluent;
+using System.Collections.Generic;
 using HSNCustomizations.DAC;
 
 namespace PX.Objects.CR
 {
     public class QuoteMaint_Extension : PXGraphExtension<QuoteMaint>
     {
+        public const string QuoteMyRptID = "LM604500";
+
         #region Selects
         public SelectFrom<LUMHSNSetup>.View HSNSetupView;
         public SelectFrom<LUMOpprTermCond>.Where<LUMOpprTermCond.quoteID.IsEqual<CRQuote.quoteID.FromCurrent>>.View TermsConditions;
+        #endregion
+
+        #region Override Methods
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Base.actionsFolder.AddMenuAction(printQuoteMY, nameof(Base.PrintQuote), true);
+        }
+        #endregion
+
+        #region Actions
+        public PXAction<CRQuote> printQuoteMY;
+        [PXButton()]
+        [PXUIField(DisplayName = "Print Quote-MY", MapEnableRights = PXCacheRights.Select)]
+        protected virtual void PrintQuoteMY()
+        {
+            if (Base.Quote.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+                    [nameof(CRQuote.OpportunityID)] = Base.Quote.Current.OpportunityID,
+                    [nameof(CRQuote.QuoteNbr)]      = Base.Quote.Current.QuoteNbr
+                };
+
+                throw new PXReportRequiredException(parameters, QuoteMyRptID, QuoteMyRptID) { SeparateWindows = true };
+            }
+        }
         #endregion
 
         #region Event Handlers
