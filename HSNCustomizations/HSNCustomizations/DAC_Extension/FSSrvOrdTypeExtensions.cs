8c75308ddd17080323c2b81725064963ffa4bfd4
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-11-16 08:01:51 +0000

    Add new two fields/columns according to spec [Design Concept - Service Order Enhancement-V3.1] 1.3.1

--- HSNCustomizations/HSNCustomizations/DAC_Extension/FSSrvOrdTypeExtensions.cs
+++ HSNCustomizations/HSNCustomizations/DAC_Extension/FSSrvOrdTypeExtensions.cs
@@ -1,19 +1,28 @@
 ﻿using PX.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PX.Objects.FS
 {
-    public class FSSrvOrdTypeExtensions : PXCacheExtension<FSSrvOrdType>
+    public class FSSrvOrdTypeExt : PXCacheExtension<FSSrvOrdType>
     {
-        #region EnableEquipmentMandatory
+        #region UsrEnableEquipmentMandatory
         [PXDBBool()]
-        [PXUIField(DisplayName = "Target Equipment ID is mandatory")]
+        [PXUIField(DisplayName = "Target Equipment ID Is Mandatory")]
         public virtual bool? UsrEnableEquipmentMandatory { get; set; }
         public abstract class usrEnableEquipmentMandatory : PX.Data.BQL.BqlBool.Field<usrEnableEquipmentMandatory> { }
-        #endregion
-    }
+		#endregion
+
+		#region UsrOnStaffIsMandStartAppt
+		[PXDBBool()]
+		[PXUIField(DisplayName = "Staff Is Mandatory When Start Appointment")]
+		public virtual bool? UsrOnStaffIsMandStartAppt { get; set; }
+		public abstract class usrOnStaffIsMandStartAppt : PX.Data.BQL.BqlBool.Field<usrOnStaffIsMandStartAppt> { }
+		#endregion
+
+		#region UsrBringBrandAttr2Txfr
+		[PXDBBool()]
+		[PXUIField(DisplayName = "Bring Brand Attribute To Transfer")]
+		public virtual bool? UsrBringBrandAttr2Txfr { get; set; }
+		public abstract class usrBringBrandAttr2Txfr : PX.Data.BQL.BqlBool.Field<usrBringBrandAttr2Txfr> { }
+		#endregion
+	}
 }
