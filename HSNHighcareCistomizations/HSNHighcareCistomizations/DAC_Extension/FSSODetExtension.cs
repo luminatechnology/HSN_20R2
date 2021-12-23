5bc2f12f4370136551dae2ac767d4ebf24b5d6d9
WayneLin <wayne.lin@lumina-tech.com>
2021-12-23 13:17:42 +0800

    1.Service order /Appointment Equipment篩選 2.HSN Preference xml fix 3.Change GI screen ID & Endpoint

--- HSNHighcareCistomizations/HSNHighcareCistomizations/DAC_Extension/FSSODetExtension.cs
+++ HSNHighcareCistomizations/HSNHighcareCistomizations/DAC_Extension/FSSODetExtension.cs
@@ -12,7 +12,6 @@ using static PX.Objects.FS.FSSODet;
 
 namespace PX.Objects.FS
 {
-    //[PXTable(IsOptional = true)]
     public class FSSODetExtension : PXCacheExtension<FSSODet>
     {
         public static bool IsActive()
@@ -24,14 +23,23 @@ namespace PX.Objects.FS
         [PXDBInt]
         [PXUIField(DisplayName = "Target Equipment ID", FieldClass = FSSetup.EquipmentManagementFieldClass)]
         [PXUIEnabled(typeof(Where<Current<isTravelItem>, NotEqual<True>>))]
-        [FSSelectorMaintenanceEquipment(typeof(FSServiceOrder.srvOrdType),
-                                        typeof(FSServiceOrder.billCustomerID),
-                                        typeof(FSServiceOrder.customerID),
-                                        typeof(FSServiceOrder.locationID),
-                                        typeof(FSServiceOrder.branchID),
-                                        typeof(FSServiceOrder.branchLocationID), Filterable = true)]
+        [PXSelector(typeof(SelectFrom<FSEquipment>
+                           .InnerJoin<FSSrvOrdType>.On<FSSrvOrdType.srvOrdType.IsEqual<FSServiceOrder.srvOrdType.FromCurrent>>
+                           .CrossJoin<FSSetup>.SingleTableOnly
+                           .Where<FSEquipment.requireMaintenance.IsEqual<True>
+                               .And<FSSetup.enableAllTargetEquipment.IsEqual<True>>
+                               .And<FSEquipment.ownerID.IsEqual<FSServiceOrder.customerID.FromCurrent>>>
+                           .SearchFor<FSEquipment.SMequipmentID>),
+                    typeof(FSEquipment.refNbr),
+                    typeof(FSEquipment.descr),
+                    typeof(FSEquipment.serialNumber),
+                    typeof(FSEquipment.ownerType),
+                    typeof(FSEquipment.ownerID),
+                    typeof(FSEquipment.locationType),
+                    typeof(FSEquipment.status),
+            SubstituteKey = typeof(FSEquipment.refNbr))]
         [PXRestrictor(typeof(Where<FSEquipment.status, Equal<EPEquipmentStatus.EquipmentStatusActive>>),
-                        TX.Messages.EQUIPMENT_IS_INSTATUS, typeof(FSEquipment.status))]
+                       TX.Messages.EQUIPMENT_IS_INSTATUS, typeof(FSEquipment.status))]
         public virtual int? SMEquipmentID { get; set; }
         #endregion
 
